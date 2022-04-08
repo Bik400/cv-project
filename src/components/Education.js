dc7fd8bb012fa9a9df109ae7a418a11eac4822f2
Bik400 <biksingh30@gmail.com>
2022-04-07 19:09:20 -0700

    Added the coursework input

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
-class Education extends Component {
 
+class Education extends Component { 
     render() {
         return (
             <div className="container">
@@ -10,6 +10,8 @@ class Education extends Component {
                 <input type="text" placeholder="School Name" value={this.props.educationInfo.school} onChange={this.props.handleSchool}></input>
                 <br></br>
                 <input type="text" placeholder="Major" value={this.props.educationInfo.major} onChange={this.props.handleMajor}></input>
+                <br></br>
+                <input type="text" placeholder="Add Coursework" className="coursework-input" onChange={this.props.handleCoursework}></input>
                 <br></br>
                 <input type="text" placeholder="Graduation Date" value={this.props.educationInfo.graduation} onChange={this.props.handleGraduation}></input>
             </div>
